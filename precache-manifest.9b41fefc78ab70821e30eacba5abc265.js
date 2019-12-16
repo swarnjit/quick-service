6e0bd7e298e9151cf9117a03b961873fb41a4e50
swarnjit <swarnjitchahal@gmail.com>
2019-12-16 15:32:21 -0500

    Updates

---
+++ precache-manifest.9b41fefc78ab70821e30eacba5abc265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1508ec84371f01030e6f5c45fe6e6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dca1eac84437fb373c2",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "18ab19fda7b244dbdf27",
+    "url": "/static/js/2.f5ac2b50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f5ac2b50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1dca1eac84437fb373c2",
+    "url": "/static/js/main.02fa811c.chunk.js"
+  },
+  {
+    "revision": "c88edd0d2fe21a7eef56",
+    "url": "/static/js/runtime-main.44868730.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
