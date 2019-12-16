0a30dd634ac567c4c453c003fff3922302261ddf
swarnjit <swarnjitchahal@gmail.com>
2019-12-16 15:34:17 -0500

    Updates

---
+++ precache-manifest.ec22d447053d0d801399dd707c8173dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35193c90fa646ca4623fb0eb577dccb",
+    "url": "/quick-service/index.html"
+  },
+  {
+    "revision": "2967a307756eab264606",
+    "url": "/quick-service/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "18ab19fda7b244dbdf27",
+    "url": "/quick-service/static/js/2.f5ac2b50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quick-service/static/js/2.f5ac2b50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2967a307756eab264606",
+    "url": "/quick-service/static/js/main.0bcfc734.chunk.js"
+  },
+  {
+    "revision": "1b209f6c22ef934864f0",
+    "url": "/quick-service/static/js/runtime-main.813fe814.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quick-service/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b41fefc78ab70821e30eacba5abc265.js"
+  "/quick-service/precache-manifest.ec22d447053d0d801399dd707c8173dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quick-service/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
